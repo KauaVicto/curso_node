947553656947567d7da860dfadf3fd0f993d743e
KauaVicto <kauavictor259@gmail.com>
2023-03-01 17:14:19 -0300

    corrigido github actions

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -6,7 +6,7 @@ import { CidadesController } from './../controllers';
 const router = Router();
 
 router.get('/', (req, res) => {
-    return res.send('Opa meu bom!!!!');
+    return res.send('Opa meu bom!!!!!');
 });
 
 router.post('/cidades', CidadesController.createBodyValidator, CidadesController.create);
