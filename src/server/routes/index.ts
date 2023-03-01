0bbe084226ad4ac9109c310c68cfd671a8004e30
Kaua Victor <kauavictor259@gmail.com>
2023-03-01 19:17:03 -0300

    layla

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -6,7 +6,7 @@ import { CidadesController } from './../controllers';
 const router = Router();
 
 router.get('/', (req, res) => {
-    return res.send('Layla, te amoo!!!❤');
+    return res.send('Layla, te amoo!!! Meu amor por você vale mais que tudo na vida❤');
 });
 
 router.post('/cidades', CidadesController.createBodyValidator, CidadesController.create);
