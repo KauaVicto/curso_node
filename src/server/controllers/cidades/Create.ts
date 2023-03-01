8da19b777b51cf3f6a27a317429f10031e07f2ef
Kaua Victor <kauavictor259@gmail.com>
2023-03-01 13:04:59 -0300

    commit initial

---
+++ src/server/controllers/cidades/Create.ts
@@ -0,0 +1,27 @@
+import { Request, RequestHandler, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
+
+import { validation } from '../../shared/middleware';
+
+interface ICidade {
+    nome: string;
+    estado: string;
+}
+
+const cidadeValidation = yup.object().shape({
+    nome: yup.string().required().min(3),
+    estado: yup.string().required().min(3),
+});
+
+export const createBodyValidator = validation('body', cidadeValidation);
+
+
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+
+    console.log(req.body);
+
+    return res.send('Cidade criada com sucesso!');
+
+};
---
+++ src/server/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import { CidadesController } from './../controllers';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    return res.send('Opa meu bom');
+});
+
+router.post('/cidades', CidadesController.createBodyValidator, CidadesController.create);
+
+export { router };
